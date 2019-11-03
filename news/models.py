9c682263ed24dbf9a121890050059b4f32d221bd
handeanup <handeanup@gmail.com>
2019-11-03 16:58:20 +0530

    added parser to parse RSS feed

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class New(models.Model):
+    title = models.CharField(max_length=500,blank=False)
+    link = models.URLField(max_length=200)
+    guid = models.TextField()
+    publish_date = models.CharField(max_length=50)
+    source_title = models.CharField(max_length=50,blank=True)
+    source_link = models.URLField(max_length=200)
+    description = models.TextField()    
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("News_detail", kwargs={"pk": self.pk})
